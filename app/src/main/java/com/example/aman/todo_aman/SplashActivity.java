4b6bca551d66343c10f69b55c2b58709ee56a884
Aman Manandhar <amanandhar0@gmail.com>
2018-12-28 18:55:20 +0545

    Splash - Login Designed

--- app/src/main/java/com/example/aman/todo_aman/SplashActivity.java
+++ app/src/main/java/com/example/aman/todo_aman/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.example.aman.todo_aman;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        },3000);
     }
 }
