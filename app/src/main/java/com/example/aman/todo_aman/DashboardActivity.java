213e7f7205d77dc7e8e2068b087e345a14c3a3bd
Aman Manandhar <amanandhar0@gmail.com>
2018-12-28 19:43:05 +0545

    Successful login and admin credentioals changes

---
+++ app/src/main/java/com/example/aman/todo_aman/DashboardActivity.java
@@ -0,0 +1,23 @@
+package com.example.aman.todo_aman;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        @SuppressLint("WrongConstant")SharedPreferences pref=getSharedPreferences("usname",MODE_PRIVATE);
+        String username=pref.getString("android:username","");
+        String password=pref.getString("android:password","");
+        TextView displayusername=findViewById(R.id.disuname);
+        displayusername.setText(username);
+        TextView displaypassword=findViewById(R.id.dispassword);
+        displaypassword.setText(password);
+    }
+}
--- app/src/main/java/com/example/aman/todo_aman/LoginActivity.java
+++ app/src/main/java/com/example/aman/todo_aman/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.example.aman.todo_aman;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +15,42 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+    public void openActivity(View view) {
+        @SuppressLint("WrongConstant") SharedPreferences pref = getSharedPreferences("usname", MODE_PRIVATE);
+//        SharedPreferences.Editor editor=pref.edit();
+        EditText uname = findViewById(R.id.username_editext);
+        EditText pass = findViewById(R.id.password_editext);
+        String username = uname.getText().toString();
+        String password = pass.getText().toString();
+
+        if (username.equals("amdin") && password.equals("admin")) {
+
+            Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
+            startActivity(intent);
+
+        } else {
+            if (!pref.contains("username")) {
+                SharedPreferences.Editor editor = pref.edit();
+                editor.putString("username", username);
+                editor.putString("password", password);
+                editor.commit();
+                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(intent);
+
+            } else {
+                String usr = pref.getString("username", "");
+                String pss = pref.getString("password", "");
+                if (username.equals(usr) && password.equals(pss)) {
+                    Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                }
+            }
+//        editor.putString("android:username",username);
+//        editor.putString("android:password",password);
+//        editor.commit();
+//
+        }
+
+    }
+
 }
