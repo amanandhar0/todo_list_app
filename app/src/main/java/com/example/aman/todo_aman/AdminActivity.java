c13e18969cf2477c307bcd694a9feef895ac329b
Aman Manandhar <amanandhar0@gmail.com>
2019-01-01 14:24:12 +0545

    Sharedp

--- app/src/main/java/com/example/aman/todo_aman/AdminActivity.java
+++ app/src/main/java/com/example/aman/todo_aman/AdminActivity.java
@@ -7,7 +7,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 public class AdminActivity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/com/example/aman/todo_aman/LoginActivity.java
+++ app/src/main/java/com/example/aman/todo_aman/LoginActivity.java
@@ -7,50 +7,89 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText loginUsername;
+    private EditText loginPassword;
+    private Button loginBttn;
 
+    private static String usernameCredential = "admin";
+    private static String passwordCredential = "admin";
+
+    SharedPreference sharedPreference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginUsername=findViewById(R.id.username_editext);
+        loginPassword=findViewById(R.id.password_editext);
+
+        loginBttn=findViewById(R.id.login_btn);
+        loginBttn.setOnClickListener(this);
+
+        sharedPreference=new SharedPreference(this);
     }
-    public void openActivity(View view) {
-        @SuppressLint("WrongConstant") SharedPreferences pref = getSharedPreferences("usname", MODE_PRIVATE);
-//        SharedPreferences.Editor editor=pref.edit();
-        EditText uname = findViewById(R.id.username_editext);
-        EditText pass = findViewById(R.id.password_editext);
-        String username = uname.getText().toString();
-        String password = pass.getText().toString();
 
-        if (username.equals("admin") && password.equals("admin")) {
+    @Override
+    public void onClick(View v) {
+        String username;
+        String password;
 
-            Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
+        username=loginUsername.getText().toString();
+        password=loginPassword.getText().toString();
+
+        if(username.equals(usernameCredential)&& password.equals(passwordCredential)){
+            Intent intent=new Intent(this,UserDetailActivity.class);
             startActivity(intent);
 
-        } else {
-            if (!pref.contains("username")) {
-                SharedPreferences.Editor editor = pref.edit();
-                editor.putString("username", username);
-                editor.putString("password", password);
-                editor.commit();
-                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
-                startActivity(intent);
-
-            } else {
-                String usr = pref.getString("username", "");
-                String pss = pref.getString("password", "");
-                if (username.equals(usr) && password.equals(pss)) {
-                    Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
-                    startActivity(intent);
-                }
-            }
-//        editor.putString("android:username",username);
-//        editor.putString("android:password",password);
-//        editor.commit();
-//
         }
-
+        else if(!username.isEmpty()&& !password.isEmpty() && username.equals(sharedPreference.getUsername()) && password.equals(sharedPreference.getPassword())){
+            Intent intent=new Intent(this,DashboardActivity.class);
+            Toast.makeText(this,"Welcome "+ username, Toast.LENGTH_SHORT).show();
+            startActivity(intent);
+            finish();
+        }
+        else
+            Toast.makeText(this,"Invalid Entry", Toast.LENGTH_SHORT).show();
     }
-
+//    public void openActivity(View view){
+//        @SuppressLint("WrongConstant") SharedPreferences pref= getSharedPreferences("usname",MODE_PRIVATE);
+//        EditText uname= findViewById(R.id.username_editext);
+//        EditText pass= findViewById(R.id.password_editext);
+//        String username=uname.getText().toString();
+//        String password=pass.getText().toString();
+//        if(username.equals("admin")&&password.equals("admin")){
+//            Intent intent = new Intent(LoginActivity.this, AdminActivity.class);
+//            startActivity(intent);
+//        }
+//        else{
+//            if(!pref.contains("username")){
+//                SharedPreferences.Editor editor=pref.edit();
+//                editor.putString("username",username);
+//                editor.putString("password",password);
+//                editor.commit();
+//                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+//                startActivity(intent);
+//            }
+//            else {
+//                String usr=pref.getString("username","");
+//                String pss=pref.getString("password","");
+//                if(username.equals(usr)&&password.equals(pss)) {
+//                    Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+//                    startActivity(intent);
+//                }
+//            }
+//
+//        }
+//
+//    }
 }
